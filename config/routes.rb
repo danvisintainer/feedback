ee1ae9c14bbc8c8947eeb9017610ceabdeb6d5ae
danvisintainer <dan@danvisintainer.com>
2015-04-06 16:45:15 -0400

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'tracks#new'
+
+  post '/create', to: 'tracks#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
