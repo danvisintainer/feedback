bc780ffb4b16317ae6248bc26df37a5d92fc22a0
danvisintainer <dan@danvisintainer.com>
2015-04-08 12:12:21 -0400

    remove unnecessary migration

--- db/migrate/20150408141456_add_columns_to_projects.rb
+++
@@ -1,9 +0,0 @@
-class AddColumnsToProjects < ActiveRecord::Migration
-  def change
-    add_column :projects, :name, :string
-    add_column :projects, :description, :string
-    add_column :projects, :user_id, :integer
-
-
-  end
-end
