b8a9b44bd1dddbc2599c231e28871afa71fe9ae6
EricFries <ericfries@gmail.com>
2015-04-10 13:04:10 -0400

    fix link bug on track partial

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-# ActiveRecord::Schema.define(version: 20150409220800) do
-
-ActiveRecord::Schema.define(version: 20150409220715) do
+ActiveRecord::Schema.define(version: 20150409220800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
