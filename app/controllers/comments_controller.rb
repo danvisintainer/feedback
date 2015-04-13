772ce776721d687664639a3599f331da67ad4002
MStines007 <mstines007@gmail.com>
2015-04-13 15:57:47 -0400

    fixed comments ajax clear field and anchor

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,11 +4,12 @@ class CommentsController < ApplicationController
     @comment = @project.comments.create(comment_params)
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment.post, notice: 'Comment was successfully created.' }
+        
+        # format.html { redirect_to @comment.post, notice: 'Comment was successfully created.' }
         format.js   { }
       #   format.json { render :show, status: :created, location: @comment }
       # else
-        format.html { render :new }
+        # format.html { render :new }
         # format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end    
