57d0524f9a47d921a58844f5714105e69343929a
EricFries <ericfries@gmail.com>
2015-04-13 13:13:18 -0400

    make deletion of project delete comments & tracks

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ActiveRecord::Base
-  has_many :comments
-  has_many :tracks
+  has_many :comments, dependent: :destroy
+  has_many :tracks, dependent: :destroy
   has_one :instrument_need
   belongs_to :user
 end
