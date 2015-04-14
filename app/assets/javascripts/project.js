8e8839d6e0d5bb6e7254d52bea73c5813ff41971
danvisintainer <dan@danvisintainer.com>
2015-04-14 10:09:02 -0400

    change proj completed status to only call at proj show

--- app/assets/javascripts/project.js
+++ app/assets/javascripts/project.js
@@ -1,10 +1,12 @@
-
-$(document).ready(function(){
+function completedCheck(){
 
 	if (gon.project_completed === true){
 		$("#recorderStart").hide();
 		$("#instrument-need-checkbox").hide();
 	}
+}
+
+$(document).ready(function(){
 	$("#collab-btn").on('submit', function(e){
 		e.preventDefault();
 		var url = $(this).attr("action");
