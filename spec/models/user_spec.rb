c16d70838b99e66e3e15c401ae9d0620bd69fde6
EricFries <ericfries@gmail.com>
2015-04-17 15:12:11 -0400

    add tests to user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) {@user = User.new(:name => "Alex", :primary_instrument => "Drums")}
+
+  it "has a name" do
+   expect(@user.name).to eq("Alex")
+  end
+
+  it "has a primary instrument" do
+   expect(@user.primary_instrument).to be_truthy
+  end
+
+  it "can't be created without a name" do
+    expect {User.new(:name => nil)}.to raise_error
+  end
+
+  it "can have projects" do 
+    p = User.reflect_on_association(:projects)
+    p.macro.should == :has_many
+  end
 end
