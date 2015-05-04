5e57dc2b62ee115283cfc407b4533cd9d691d3c9
EricFries <ericfries@gmail.com>
2015-05-04 14:12:12 -0400

    add instance method tests to project

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :project do
-    
+    name { Faker::Name.name }
   end
 
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,8 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
+
   context "Instance Methods" do
     
+    let(:project) { FactoryGirl.build(:project)}
+
+    it "has a valid factory" do
+      FactoryGirl.create(:project).should be_valid
+    end
+
+    it "has a name" do
+      expect(project.name).to be_truthy
+    end
+
+    it "is invalid without a name" do
+      expect(project.update(:name => nil)).to be(false)
+    end
+
+    it "has a description" do
+      expect(project.update(:description => "Hoodie pork belly freegan typewriter cronut Shoreditch. Tilde VHS jean shorts, Echo Park banjo messenger bag bicycle rights gastropub Tumblr you probably haven't heard of them pop-up mustache.")).to be(true)
+    end
+
   end
 
   context "Associations" do
