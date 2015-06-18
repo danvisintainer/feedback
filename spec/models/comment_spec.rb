f1ddee99f94a93c15fcd447ffab13d7b44134aea
EricFries <ericfries@gmail.com>
2015-06-18 14:18:32 -0400

    remove coffeescript files

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Associations" do
+    it "belongs to a project" do
+      c = Comment.reflect_on_association(:project)
+      expect(c.macro).to eq(:belongs_to)
+    end
+    it "belongs to a user" do
+      c = Comment.reflect_on_association(:user)
+      expect(c.macro).to eq(:belongs_to)
+    end
+  end
+
 end
--- spec/models/instrument_need_spec.rb
+++ spec/models/instrument_need_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe InstrumentNeed, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context "Instance Methods" do
+    it "can have guitar, bass, drums, keyboard, vocals, saxophone or other needs" do
+      instrument_need = InstrumentNeed.new
+      expect(instrument_need.guitar).to be_truthy
+      expect(instrument_need.bass).to be_truthy
+      expect(instrument_need.drums).to be_truthy
+      expect(instrument_need.keyboards).to be_truthy
+      expect(instrument_need.vocals).to be_truthy
+      expect(instrument_need.saxophone).to be_truthy
+      expect(instrument_need.other).to be_truthy
+    end
+    
+  end
+
+  context "Associations" do
+    it 'belongs to a project' do
+    i = InstrumentNeed.reflect_on_association(:project)
+    expect(i.macro).to eq(:belongs_to)
+    end
+  end
 end
